d1c3a9d596d3d8835ff49f1466cf114559540e67
orutanakaito <voltgt777@gmail.com>
2024-09-03 21:56:03 +0900

    プレイヤーのステータス表示させました。

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,31 @@
 
 int main() {
 
-    Player* player = new Player[4]();
+    Player* player = new Player[5]();
 
     player[0].setName('h');
     player[1].setName('e');
     player[2].setName('r');
     player[3].setName('o');
+    player[4].setName(' ');
 
-    for (int i = 0; i < 4; i++) {
-        std::cout << player[i].getName() << std::endl;
+    std::cout << "-----------" << ' ' << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << player[i].getName();
     }
 
     player->setHp(100);
     int hp = player->getHp();
-    std::cout << hp << std::endl;
+    std::cout << "HP" << ' ' << hp << std::endl;
+
+    std::cout << "-----------" << ' ' << std::endl;
 
     player->setLv(5);
     int lv = player->getLv();
-    std::cout << lv << std::endl;
+    std::cout << "LV" << ' ' << lv << std::endl;
+
+    std::cout << "-----------" << ' ' << std::endl;
+
     return 0;
 }
