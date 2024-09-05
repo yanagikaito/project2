59e36d555872f1d8855d0a984efc67404dcfc59b
orutanakaito <voltgt777@gmail.com>
2024-09-05 20:46:15 +0900

    オーバライド正しく実装されていなかったのでplayerクラスでオーバライドしました。

--- Character.h
+++ Character.h
@@ -1,5 +1,5 @@
 class Character {
-private:
+protected:
     char Name;
     int Hp;
     int Lv;
@@ -8,10 +8,8 @@ public:
     std::string name;
 
 public:
-    void setName(char name);
-    char getName();
-    void setHp(int hp);
-    int getHp();
-    void setLv(int lv);
-    int getLv();
+    virtual void setHp(int hp) = 0;
+    virtual int getHp() = 0;
+    virtual void setLv(int lv) = 0;
+    virtual int getLv() = 0;
 };
