ab905cfddcf9a7328479d5532218103e4d33346c
orutanakaito <voltgt777@gmail.com>
2024-09-07 11:52:48 +0900

    基底クラスのコンストラクタの初期化とステータス表示する関数を変更しました。

--- Character.h
+++ Character.h
@@ -19,26 +19,14 @@ public:
     Character() { _name = "名無し"; _hp = 0; _lv = 0; }
 
     // 引数ありのコンストラクタ関数
-    Character(string name, int hp, int lv) { _name = name; _hp = hp; _lv = lv; }
+    Character(string name, int hp, int lv) :
+        _name(name), _hp(hp), _lv(lv) {}
 
     // デストラクタ
     ~Character() { cout << _name << "が消えました。" << endl; }
 
     // ステータス表示する関数
-    virtual void putStatus() {
-
-        std::cout << "---------------" << ' ' << std::endl;
-
-        std::cout << _name << ' ';
-
-        std::cout << "HP" << ' ' << _hp << std::endl;
-
-        std::cout << "---------------" << ' ' << std::endl;
-
-        std::cout << "LV" << ' ' << _lv << std::endl;
-
-        std::cout << "---------------" << ' ' << std::endl;
-    }
+    virtual void putStatus() = 0;
 
     // ゲッター
     string getName() { return _name; }
