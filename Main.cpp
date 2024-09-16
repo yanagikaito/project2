962acfead2d187d434a5b4c72dc3bcf321c63dd6
orutanakaito <voltgt777@gmail.com>
2024-09-16 15:21:35 +0900

    adjacent_findは同じ値が連続している最初の要素を検索する。greaterは、右辺より左辺の値が大きいとtrueを返す

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 #include "Character.h"
 #include "Player.h"
 #include "Monster.h"
 #include "Boss.h"
 
-void print_vector_info(const std::vector<int>& v) {
-    std::cout << "size = " << v.size() << ", capacity = " << v.capacity() << std::endl;
-}
 int main() {
 
-    std::vector<int> numbers = { 3,1,-4,1,-5,9,-2,6,5,3 };
-    print_vector_info(numbers);
+    std::vector<int> numbers = { 1, 3, 3, 5, 0, 4, 5, 2 };
 
-    /*numbers.reserve();*/
-    std::cout << "------------------" << std::endl;
-    print_vector_info(numbers);
-    std::cout << "------------------" << std::endl;
+    // 同じ値が連続している最初の要素を検索する
+    auto it = std::adjacent_find(numbers.begin(), numbers.end());
 
-    for (int i = 0; i < 5; i++) {
-        numbers.emplace_back(i);
-        print_vector_info(numbers);
-        std::cout << "------------------" << std::endl;
+    if (it == numbers.end()) {
+        std::cout << "not found" << std::endl;
     }
-    print_vector_info(numbers);
+
+    else {
+
+        std::cout << "found: index==" << std::distance(numbers.begin(), it) << std::endl;
+        std::cout << "found: index==" << std::distance(numbers.begin(), it) << ", num==" << *it << ", next num==" << *(it + 1) << std::endl;
+        std::cout << std::boolalpha << "*it == *(it+1): " << (*it == *(it + 1)) << std::endl;
+
+        // std::greaterは右辺より左辺の値が大きいとtrueを返す
+        std::cout << std::boolalpha << std::greater<int>()(3, 3) << std::endl;
+        std::cout << std::boolalpha << std::greater<int>()(5, 3) << std::endl;
+    }
 
     return 0;
 }
