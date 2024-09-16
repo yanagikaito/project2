a1ef65bd0671aa0f2a49f776939d483d989c58c2
orutanakaito <voltgt777@gmail.com>
2024-09-16 21:26:19 +0900

    1から6までランダムに表示するコードと毎回固定で表示するコード書きました。

--- Main.cpp
+++ Main.cpp
@@ -2,32 +2,101 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <random>
 #include "Character.h"
 #include "Player.h"
 #include "Monster.h"
 #include "Boss.h"
 
+//template < typename Engine, typename Distribution>
+//void f(Engine& e)
+//{
+//    // 最小値
+//    auto a = e.min(e);
+//    // 最大値
+//    auto b = e.max(e);
+//    // 乱数
+//    auto r1 = e(e);
+//    // 次の乱数
+//    auto r2 = e(e);
+//}
+
 int main() {
 
-    std::vector<int> numbers = { 1, 3, 3, 5, 0, 4, 5, 2 };
+    // ランダムデバイスを使ってシード値を生成
+    std::random_device rd;
 
-    // 同じ値が連続している最初の要素を検索する
-    auto it = std::adjacent_find(numbers.begin(), numbers.end());
+    // シード値を使って乱数生成器を初期化
+    std::mt19937 engine(rd());
 
-    if (it == numbers.end()) {
-        std::cout << "not found" << std::endl;
+    // 1から6の範囲の乱数を生成する分布を作成
+    std::uniform_int_distribution<int> dist(1, 6);
+
+    // 乱数を生成して表示
+    std::cout << "ランダム値: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << dist(engine) << " ";
     }
+    std::cout << std::endl;
 
-    else {
+    // シード値のベクターを作成
+    std::vector<std::uint32_t> v = { 1,2,3,4,5 };
 
-        std::cout << "found: index==" << std::distance(numbers.begin(), it) << std::endl;
-        std::cout << "found: index==" << std::distance(numbers.begin(), it) << ", num==" << *it << ", next num==" << *(it + 1) << std::endl;
-        std::cout << std::boolalpha << "*it == *(it+1): " << (*it == *(it + 1)) << std::endl;
+    // シード列オブジェクトを作成
+    std::seed_seq s2(std::begin(v), std::end(v));
 
-        // std::greaterは右辺より左辺の値が大きいとtrueを返す
-        std::cout << std::boolalpha << std::greater<int>()(3, 3) << std::endl;
-        std::cout << std::boolalpha << std::greater<int>()(5, 3) << std::endl;
+    // シード列オブジェクトを使って乱数生成器を初期化
+    std::mt19937 engine2(s2);
+
+    std::cout << "固定値: ";
+
+    // 乱数を生成して表示
+    for (int i = 0; i < 5; ++i) {
+        std::cout << dist(engine2) << " ";
     }
+    std::cout << std::endl;
+
+
+    // メルセンヌツイスターというアルゴリズムを実装した乱数エンジン
+    std::seed_seq s{ 124 };
+    std::mt19937 e(s);
+
+    // 乱数を生成
+   // 内部状態が変わる
+    auto r1 = e();
+
+    // シード値を設定
+    e.seed(s);
+
+    // 乱数を生成
+    auto r2 = e();
+
+    std::cout << r1 << std::endl;
+    std::cout << r2 << std::endl;
+
+    // std::mt19937のオブジェクトサイズは5000バイト
+    std::cout << sizeof(std::mt19937);
+
+    // 同じシード値による乱数は同じ値になる
+    // r1 == r2
+    // 乱数分布
+    // 生の乱数を望みの範囲の乱数に加工するためのライブラリ
+    //std::uniform_int_distribution<int>d(1, 6);
+
+    //auto a = d(e); // 1回目
+    //auto b = d(e); // 2回目
+
+    //auto result = a - b; // 結果
+
+    //std::cout << a;
+    //std::cout << b;
+    //std::cout << result;
+
+    //for (int i = 0; i != 10; ++i)
+    //{
+    //    // 乱数を出力
+    //    std::cout << d(e) << std::endl;
+    //}
 
     return 0;
 }
